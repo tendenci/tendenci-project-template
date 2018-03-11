cfdaad042e5b2a0f277d125c139a62b1e2d5092e
Paul Donohue <git@PaulSD.com>
2018-03-11 01:41:55 -0500

    Fix urlpattern RemovedInDjango110Warnings

--- conf/urls.py
+++ conf/urls.py
@@ -1,25 +1,25 @@
+from django.conf.urls import url
 from django.conf import settings
-from django.conf.urls import *
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views import static
 from tendenci.urls import urlpatterns as tendenci_urls
 
 handler500 = 'tendenci.apps.base.views.custom_error'
 
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = staticfiles_urlpatterns()
 
 if not settings.USE_S3_STORAGE:
-    urlpatterns += patterns('',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', static.serve, {
             'document_root': settings.MEDIA_ROOT,
         }),
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        url(r'^static/(?P<path>.*)$', static.serve, {
             'document_root': settings.STATIC_ROOT,
         }),
-        url(r'^themes/(?P<path>.*)$', 'django.views.static.serve', {
+        url(r'^themes/(?P<path>.*)$', static.serve, {
             'document_root': settings.THEMES_DIR,
         }),
-    )
+    ]
 
 # Local url patterns for development
 try:
