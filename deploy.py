1755dfe602c9ba8dd80e3b2f2dcd55421f42a943
Paul Donohue <git@PaulSD.com>
2018-03-24 20:10:06 -0400

    Move deploy.py from tendenci-project-template to tendenci

--- deploy.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-subprocess.check_call(['python', 'manage.py', 'collectstatic', '--noinput'])
-subprocess.check_call(['python', 'manage.py', 'update_settings'])
-subprocess.check_call(['python', 'manage.py', 'clear_theme_cache'])
-subprocess.check_call(['python', 'manage.py', 'populate_default_entity'])
-subprocess.check_call(['python', 'manage.py', 'populate_entity_and_auth_group_columns'])
-subprocess.check_call(['python', 'manage.py', 'loaddata', 'initial_data.json'])
